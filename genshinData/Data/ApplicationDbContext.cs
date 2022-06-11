6e3d5823694aba9a58586041e6b2c4975459fcfa
PaulHaig <gaagpavel01@gmail.com>
2022-06-11 21:31:59 +0300

    Добавил контекст и настроил проект
    Добавил класс контекста для последующего добавления классов модели.
    Также внёс изменения в настройки приложения и файл Program.cs.

---
+++ genshinData/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using genshinData.Models;
+
+namespace genshinData.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

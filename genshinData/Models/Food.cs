1d322efd14048c835c8f41367c451c67510223f0
PaulHaig <gaagpavel01@gmail.com>
2022-06-17 21:27:30 +0300

    Добавил Food.cs из ветки Food

---
+++ genshinData/Models/Food.cs
@@ -0,0 +1,11 @@
+﻿namespace genshinData.Models
+{
+    public class Food
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int Rarity { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
